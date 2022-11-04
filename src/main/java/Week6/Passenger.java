912dedfef4c4f397c8d881fffb4f0e1602e85bb6
PATRICK HESSION <G00394619@gmit.ie>
2022-11-04 02:34:43 +0000

    Stored title

--- src/main/java/Week6/Passenger.java
+++ src/main/java/Week6/Passenger.java
@@ -1,14 +1,10 @@
 package Week6;
 
-<<<<<<< HEAD
-public class Passenger {
-=======
 import java.util.Scanner;
 
 public class Passenger {
   Scanner input = new Scanner(System.in);
->>>>>>> bced571 (Added scanner)
-
+  String Title = input.next();
 
 
 }
