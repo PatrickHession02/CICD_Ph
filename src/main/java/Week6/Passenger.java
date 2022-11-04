18f34ac7ddbd8442288b92722d037ab6cfbbea1e
PATRICK HESSION <G00394619@gmit.ie>
2022-11-04 02:25:12 +0000

    Added scanner

--- src/main/java/Week6/Passenger.java
+++ src/main/java/Week6/Passenger.java
@@ -1,6 +1,13 @@
 package Week6;
 
+<<<<<<< HEAD
 public class Passenger {
+=======
+import java.util.Scanner;
+
+public class Passenger {
+  Scanner input = new Scanner(System.in);
+>>>>>>> bced571 (Added scanner)
 
 
 
