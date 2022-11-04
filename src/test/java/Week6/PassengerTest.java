aab48d91eeeac845c10b375432edf324f753a1e3
PATRICK HESSION <G00394619@gmit.ie>
2022-11-04 19:50:07 +0000

    Added new objects

--- src/test/java/Week6/PassengerTest.java
+++ src/test/java/Week6/PassengerTest.java
@@ -4,18 +4,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
 
     @BeforeEach
-    void setUp() {
-    }
+    void setUp() {myScan = new Scanner();}
 @Test
-void testTitle()
-{
+void testTitle() {}
 
-}
+    @Test
+
     @AfterEach
     void tearDown() {
     }
