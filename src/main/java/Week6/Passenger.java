afac08f1198dc5b088f2afdbb219d5bb678d981b
PATRICK HESSION <G00394619@gmit.ie>
2022-11-05 15:42:10 +0000

    Added getters ans setters

--- src/main/java/Week6/Passenger.java
+++ src/main/java/Week6/Passenger.java
@@ -3,12 +3,33 @@ package Week6;
 import java.util.Scanner;
 
 public class Passenger {
+    String Title;
+    String Name;
+    int Age;
+    long phoneNum;
+    long ID;
+
  public Scanner input = new Scanner(System.in);
 
-  public String Title(){
-     String Title = input.next();
+
+ public Passenger(String Title, String Name, int Age, long ID, long phoneNum)   //constructor
+ {
+     setTitle(Title);
+     setName(Name);
+     setAge(Age);
+     setID(ID);
+     setPhone(phoneNum);
+
+ }
+  public String getTitle(){
     return Title;
   }
+
+    public String setTitle(String myTitle){
+     if("Mr".equalsIgnoreCase(myTitle) || "Ms".equalsIgnoreCase(myTitle) || "Mrs".equalsIgnoreCase(myTitle))
+     { 
+        return Title;
+     }
 
   public String Name(){
     String Name = input.next();
